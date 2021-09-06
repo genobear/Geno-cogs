2af79986ab643a99c435810506d47ff1a977a09a
unknown <33195938+genobear@users.noreply.github.com>
2021-09-07 00:19:42 +0100

    initial commit

---
+++ bdb/bdb.py
@@ -0,0 +1,38 @@
+from redbot.core import commands
+
+#requirements for googlsheet integration
+import os
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+scope = ["https://spreadsheets.google.com/feeds",
+         'https://www.googleapis.com/auth/spreadsheets',
+         "https://www.googleapis.com/auth/drive.file",
+         "https://www.googleapis.com/auth/drive"]
+
+creds = ServiceAccountCredentials.from_json_keyfile_name(os.path.join(ROOT_DIR, 'client.json'), scope)
+client = gspread.authorize(creds)
+
+
+class bdb(commands.Cog):
+    """My custom cog"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def scan(self, ctx, arg):
+        """This does stuff!"""
+        # Your code will go here
+        sheet = client.open('INS').sheet1
+        sheetDetails = sheet.get_all_values()
+        IDColumn = sheet.col_values(2)
+        if arg in IDColumn:
+            for a in sheetDetails:
+                if a[1] == arg:
+                    response = ("User " + str(a[0]) + " is in these discords :\n" + a[2])
+        else:
+            response = "User not in database"
+        await ctx.send("%s" % response)
---
+++ bdb/testscan.py
@@ -0,0 +1,26 @@
+import os
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+scope = ["https://spreadsheets.google.com/feeds",
+         'https://www.googleapis.com/auth/spreadsheets',
+         "https://www.googleapis.com/auth/drive.file",
+         "https://www.googleapis.com/auth/drive"]
+
+creds = ServiceAccountCredentials.from_json_keyfile_name(os.path.join(ROOT_DIR, 'client.json'), scope)
+client = gspread.authorize(creds)
+
+sheet = client.open('INS').sheet1
+sheetDetails = sheet.get_all_values()
+IDColumn = sheet.col_values(2)
+
+discordID = input("Enter discord ID : ") #This would be the !scan userID command
+
+if discordID in IDColumn:
+    for a in sheetDetails:
+        if a[1] == discordID:
+            print("User " + str(a[0]) + " is in these discords :\n" + a[2])
+else:
+    print("User not in database")
