4f8eeaf37d8617ddd13a5ca7fecb52aee1fdd106
flowbirduk <scott.moore@flowbird.group>
2021-11-25 10:40:58 +0000

    Update bdb.py

--- bdb/bdb.py
+++ bdb/bdb.py
@@ -44,4 +44,4 @@ class bdb(commands.Cog):
     @commands.command(pass_context = True)
     async def invasion(self, ctx):
         """Time for Invasion?"""
-        await ctx.send(file=discord.File("so_we_now_have_invasion.mp3"))
+        await ctx.send(file=discord.File("/home/genobear90/share/Red-DiscordBot/data/redenv/cogs/CogManager/cogs/bdb/so_we_now_have_invasion.mp3"))
