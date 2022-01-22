0249dc83c953f57960adf3918c3d9b603569c934
genobear <scottyandrewmoore@gmail.com>
2022-01-22 11:22:14 +0000

    Update bdb.py

--- bdb/bdb.py
+++ bdb/bdb.py
@@ -683,6 +683,9 @@ class bdb(commands.Cog):
                 j = 0
         worksheet.batch_update(update)
         await ctx.send("Global List Populated")
+        await ctx.send(roleList)
+        await ctx.send(idList)
+        await ctx.send(users)
 
     # @commands.command()
     # async def tradepost(self, ctx):
