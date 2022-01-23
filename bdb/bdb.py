a14c6ec968b9769488d7ba406935bf26f1781836
Rootoo2 <47012764+Rootoo2@users.noreply.github.com>
2022-01-23 11:38:51 +0000

    Update bdb.py

--- bdb/bdb.py
+++ bdb/bdb.py
@@ -826,8 +826,11 @@ class bdb(commands.Cog):
                 update.clear()
                 j = 0  #
         updateGlobal = list(filter(None, updateGlobal))
-        dataFromGlobalList.batch_update(updateGlobal)
-        updateVersionNumber(dataFromGlobalList)
+        try:
+            dataFromGlobalList.batch_update(updateGlobal)
+            updateVersionNumber(dataFromGlobalList)
+        except Exception as e:
+            sendLog("Critical", "Tryiong to update global", updateGlobal, "830", "Event Ending", "Weird Shit updating global")
         worksheet.batch_update(update)
         self.looper.cancel()
         worksheet.update_title(str(area) + " " + str(datetime.now().strftime("%d-%m-%Y")) + " (Closed)")
