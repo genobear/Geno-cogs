6dff9a2fa6736e68e3f3fe588f9bb416aa4785fd
Rootoo2 <47012764+Rootoo2@users.noreply.github.com>
2022-01-23 17:06:26 +0000

    Update bdb.py

--- bdb/bdb.py
+++ bdb/bdb.py
@@ -983,6 +983,8 @@ class bdb(commands.Cog):
                             update.clear()
                             updateGlobalStats.clear()
                             j = 0
+                    else:
+                        rowNumber = rowNumber + 1
                 except Exception as e:
                     exc_type, exc_obj, exc_tb = sys.exc_info()
                     fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
