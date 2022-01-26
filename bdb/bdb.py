7207fb455f49716606e28ddfc0a9659c47c35dfb
Rootoo2 <47012764+Rootoo2@users.noreply.github.com>
2022-01-26 21:10:37 +0000

    Update bdb.py

--- bdb/bdb.py
+++ bdb/bdb.py
@@ -438,7 +438,7 @@ def imgProcession(img):
         zeroCorrectionList = pickle.load(fp)
 
     def processImage(crop):
-        nameOffImage = str(pytesseract.image_to_string(crop, config='--psm 6 --oem 3 digits -c tessedit_char_whitelist=0123456789')).split("\n")
+        nameOffImage = str(pytesseract.image_to_string(crop, config='--psm 6 --oem 3 -c tessedit_char_whitelist=0123456789\\')).split("\n")
         nameOffImage = list(filter(None, nameOffImage))
 
         return nameOffImage
