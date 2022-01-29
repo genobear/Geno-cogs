8a0443c814100be569c8376da7a8375faf950b52
Rootoo2 <47012764+Rootoo2@users.noreply.github.com>
2022-01-29 11:49:18 +0000

    Update bdb.py

--- bdb/bdb.py
+++ bdb/bdb.py
@@ -438,6 +438,7 @@ def imgProcession(img):
         zeroCorrectionList = pickle.load(fp)
 
     def processImage(crop):
+        tesseract.setVariable("tessedit_char_whitelist", "ABCDEFGHIJKLMNOPQRSTUVWXYZ")
         nameOffImage = str(pytesseract.image_to_string(crop, config='--oem 0 -c tessedit_char_whitelist=0123456789 --psm 6')).split("\n")
         nameOffImage = list(filter(None, nameOffImage))
 
