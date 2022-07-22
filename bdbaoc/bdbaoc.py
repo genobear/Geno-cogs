4e006d81aeedc64f913f4a7cf1270273ea283391
Rootoo2 <47012764+Rootoo2@users.noreply.github.com>
2022-07-22 19:39:26 +0100

    Update bdbaoc.py

--- bdbaoc/bdbaoc.py
+++ bdbaoc/bdbaoc.py
@@ -18,8 +18,8 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 chrome_options = Options()
-chrome_options.addArguments("--no-sandbox");
-chrome_options.addArguments("--disable-dev-shm-usage");
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--headless")
 # chrome_options.headless = True # also works
 
