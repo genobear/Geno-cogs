0690fba911f86ead012aaca6eb80cd2070bf7650
genobear <scottyandrewmoore@gmail.com>
2022-07-28 19:39:52 +0100

    Update embeds.py

--- bdbaoc/embeds.py
+++ bdbaoc/embeds.py
@@ -6,6 +6,8 @@ async def scan_embed(ctx, member: discord.Member, resultfound: bool,foundon,join
         usernames = "-"
     if nicks is "" or None:
         nicks = "-"
+
+    #SOMETHING FOUND EMBED 
     embedok = discord.Embed(
                 title="Scan Results",
                 url="https://docs.google.com/spreadsheets/d/1hph6Xpfp9zngJBMzi24MChRK5Alz5Qt4Uz1nQ8L_m84/edit#gid=0",
@@ -17,8 +19,9 @@ async def scan_embed(ctx, member: discord.Member, resultfound: bool,foundon,join
     embedok.add_field(name="Known Usernames:", value=f"{usernames}", inline="True")
     embedok.add_field(name="Known Nicknames:", value=f"{nicks}", inline="True")
     embedok.set_footer(text="Powered by Backdoor Bandito")
-    embedok.set_author(name=ctx.author.display_name, url=ctx.author.avatar_url, icon_url=ctx.author.avatar_url)
+    embedok.set_author(name=member.display_name, url=member.avatar_url, icon_url=member.avatar_url)
 
+    #USER NOT IN DATABASE EMBED
     embednok = discord.Embed(
                 title="Scan Results",
                 url="https://docs.google.com/spreadsheets/d/1hph6Xpfp9zngJBMzi24MChRK5Alz5Qt4Uz1nQ8L_m84/edit#gid=0",
@@ -26,7 +29,6 @@ async def scan_embed(ctx, member: discord.Member, resultfound: bool,foundon,join
                 color=discord.Color.random())
     embednok.set_thumbnail(url="https://cdn.discordapp.com/avatars/839574978088796210/296b1a22e987d97431902d0e1db2bae2.png")
     embednok.set_footer(text="Powered by Backdoor Bandito")
-
 
     if resultfound:
         return embedok
