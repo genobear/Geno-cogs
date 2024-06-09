83bf8829386f887b1a8c815bd8472b00e33cb62a
genobear <33195938+genobear@users.noreply.github.com>
2024-06-09 13:07:21 +0100

    Update embeds.py

--- bdbaoc/embeds.py
+++ bdbaoc/embeds.py
@@ -1,34 +1,40 @@
 import discord
 
 
-async def scan_embed(member: discord.Member, resultfound: bool,foundon,joinedat,usernames=None,nicks=None)-> discord.Embed:
+async def scan_embed(member: discord.Member, resultfound: bool, foundon, joinedat, usernames=None, nicks=None) -> discord.Embed:
     if usernames is "" or None:
         usernames = "-"
     if nicks is "" or None:
         nicks = "-"
 
-    #SOMETHING FOUND EMBED 
+    # SOMETHING FOUND EMBED
     embedok = discord.Embed(
-                title="Scan Results",
-                url="https://docs.google.com/spreadsheets/d/1hph6Xpfp9zngJBMzi24MChRK5Alz5Qt4Uz1nQ8L_m84/edit#gid=0",
-                color=discord.Color.random())
-    embedok.set_thumbnail(url="https://cdn.discordapp.com/avatars/839574978088796210/296b1a22e987d97431902d0e1db2bae2.png")
-    embedok.add_field(name="Servers Found on:", value=f"{foundon}", inline="True")
+        title="Scan Results",
+        url="https://docs.google.com/spreadsheets/d/1hph6Xpfp9zngJBMzi24MChRK5Alz5Qt4Uz1nQ8L_m84/edit#gid=0",
+        color=discord.Color.random())
+    embedok.set_thumbnail(
+        url="https://cdn.discordapp.com/avatars/839574978088796210/296b1a22e987d97431902d0e1db2bae2.png")
+    embedok.add_field(name="Servers Found on:",
+                      value=f"{foundon}", inline="True")
     embedok.add_field(name="Joined at:", value=f"{joinedat}", inline="True")
     embedok.add_field(name="-", value="-", inline="false")
-    embedok.add_field(name="Known Usernames:", value=f"{usernames}", inline="True")
+    embedok.add_field(name="Known Usernames:",
+                      value=f"{usernames}", inline="True")
     embedok.add_field(name="Known Nicknames:", value=f"{nicks}", inline="True")
     embedok.set_footer(text="Powered by Backdoor Bandito")
-    embedok.set_author(name=member.display_name, url=member.avatar_url, icon_url=member.avatar_url)
+    embedok.set_author(name=member.display_name,
+                       url=member.display_avatar.url, icon_url=member.display_avatar.url)
 
-    #USER NOT IN DATABASE EMBED
+    # USER NOT IN DATABASE EMBED
     embednok = discord.Embed(
-                title="Scan Results",
-                url="https://docs.google.com/spreadsheets/d/1hph6Xpfp9zngJBMzi24MChRK5Alz5Qt4Uz1nQ8L_m84/edit#gid=0",
-                description="User not in Database",
-                color=discord.Color.random())
-    embednok.set_author(name=member.display_name, url=member.avatar_url, icon_url=member.avatar_url)
-    embednok.set_thumbnail(url="https://cdn.discordapp.com/avatars/839574978088796210/296b1a22e987d97431902d0e1db2bae2.png")
+        title="Scan Results",
+        url="https://docs.google.com/spreadsheets/d/1hph6Xpfp9zngJBMzi24MChRK5Alz5Qt4Uz1nQ8L_m84/edit#gid=0",
+        description="User not in Database",
+        color=discord.Color.random())
+    embednok.set_author(name=member.display_name,
+                        url=member.display_avatar.url, icon_url=member.display_avatar.url)
+    embednok.set_thumbnail(
+        url="https://cdn.discordapp.com/avatars/839574978088796210/296b1a22e987d97431902d0e1db2bae2.png")
     embednok.set_footer(text="Powered by Backdoor Bandito")
 
     if resultfound:
@@ -39,7 +45,7 @@ async def scan_embed(member: discord.Member, resultfound: bool,foundon,joinedat,
 
 async def apply_embed(ctx) -> discord.Embed:
     embed = discord.Embed(
-                title="Scan Results",
-                url="https://docs.google.com/spreadsheets/d/1hph6Xpfp9zngJBMzi24MChRK5Alz5Qt4Uz1nQ8L_m84/edit#gid=0",
-                color=discord.Color.random())
+        title="Scan Results",
+        url="https://docs.google.com/spreadsheets/d/1hph6Xpfp9zngJBMzi24MChRK5Alz5Qt4Uz1nQ8L_m84/edit#gid=0",
+        color=discord.Color.random())
     return embed
